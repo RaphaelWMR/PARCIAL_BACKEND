c7997bb6ca4ba4c8c880367de3029cd652d95616
RaphaelMR <matosramosraphael@gmail.com>
2024-05-15 11:02:12 -0500

    db connected

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,8 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql'
+const database = process.env.DB_NAME;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const sequelize = new Sequelize(database, username, password, {
+    host: host,
+    dialect: 'mysql',
+    port: port
 });
 exports.default = sequelize;
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql'
+const database = process.env.DB_NAME;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+
+const sequelize = new Sequelize(database, username, password, {
+    host: host,
+    dialect: 'mysql',
+    port: port
 });
 
 export default sequelize;
